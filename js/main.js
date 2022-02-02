c6ba3667784c1217b18b555fe646690818b08177
alexnegoi09 <alexnegoi09@gmail.com>
2022-02-03 01:44:12 +0200

    refactored main.js

--- js/main.js
+++ js/main.js
@@ -17,26 +17,17 @@ window.onscroll = () => {
 
     // navbar styling
 
-    navbar.style.height = "50px";
-    navbar.style.backgroundColor = "rgba(204, 204, 204, 0.9)";
-    navbar.style.boxShadow = "1px 1px 1px black";
-    navbar.style.lineHeight = "50px";
+    navbar.classList.add('nav-styled');
 
     // navlinks styling
-
+    
     items.forEach((item) => {
-      item.style.fontSize = "1.2rem";
-      item.style.color = "inherit";
+      item.classList.add('navlink-styled');
     });
   } else {
-    navbar.style.height = "initial";
-    navbar.style.backgroundColor = "initial";
-    navbar.style.boxShadow = "none";
-    navbar.style.lineHeight = "70px";
-
+    navbar.removeAttribute('class');
     items.forEach((item) => {
-      item.style.fontSize = "1.4rem";
-      item.style.color = "white";
+      item.removeAttribute('class');
     });
   }
 }
