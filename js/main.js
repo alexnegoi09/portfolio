97b8ca8d00e82e3767abc79af6ec9c3cdf050db4
alexnegoi09 <alexnegoi09@gmail.com>
2022-05-16 15:24:19 +0300

    refactor some code in style.css, main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,30 @@
 const navbar = document.getElementById("navbar");
 const items = document.querySelectorAll("#navbar > p a");
-const mainHeader = document.querySelector("#main-image-container #main-header");
-
+const mainHeader = document.querySelector("#main-header");
 
 // main header animation
 
 window.onload = () => {
   mainHeader.style.opacity = "1";
-}
-
+};
 
 // navbar animation
 
 window.onscroll = () => {
   if (document.documentElement.scrollTop > 150) {
-
     // navbar styling
 
-    navbar.classList.add('nav-styled');
+    navbar.classList.add("nav-styled");
 
     // navlinks styling
-    
+
     items.forEach((item) => {
-      item.classList.add('navlink-styled');
+      item.classList.add("navlink-styled");
     });
   } else {
-    navbar.removeAttribute('class');
+    navbar.removeAttribute("class");
     items.forEach((item) => {
-      item.removeAttribute('class');
+      item.removeAttribute("class");
     });
   }
-}
+};
